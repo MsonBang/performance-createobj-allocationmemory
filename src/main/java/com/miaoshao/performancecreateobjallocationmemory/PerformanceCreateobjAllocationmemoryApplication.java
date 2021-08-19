06173e46317fedd49b86b616a105ece147808639
miaoxuebing <352835148@qq.com>
2021-08-19 14:03:20 +0800

    第一版笔记

---
+++ src/main/java/com/miaoshao/performancecreateobjallocationmemory/PerformanceCreateobjAllocationmemoryApplication.java
@@ -0,0 +1,13 @@
+package com.miaoshao.performancecreateobjallocationmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceCreateobjAllocationmemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceCreateobjAllocationmemoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miaoshao/performancecreateobjallocationmemory/PerformanceCreateobjAllocationmemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miaoshao.performancecreateobjallocationmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceCreateobjAllocationmemoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
